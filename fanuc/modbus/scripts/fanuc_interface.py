4eb4b26441882c4f15b7d93c1b15ed5ecc856963
rarrais <rafael.arrais@gmail.com>
2016-06-03 17:40:32 +0100

    info update

--- fanuc/modbus/scripts/fanuc_interface.py
+++ fanuc/modbus/scripts/fanuc_interface.py
@@ -4,12 +4,14 @@ import rospy
 from modbus.modbus_wrapper_client import ModbusWrapperClient 
 from std_msgs.msg import Int32MultiArray as HoldingRegister
 from robotiq_s_model_control.msg import _SModel_robot_output  as outputMsg
+from robotiq_s_model_control.msg import _SModel_robot_input  as inputMsg
 
 NUM_REGISTERS = 2
 ADDRESS_READ_START = 1
 ADDRESS_WRITE_START = 1
 
-pub = rospy.Publisher('SModelRobotOutput', outputMsg.SModel_robot_output, queue_size=10)
+pub_gripper = rospy.Publisher('SModelRobotOutput', outputMsg.SModel_robot_output, queue_size=10)
+pub_robot = rospy.Publisher("modbus_wrapper/output",HoldingRegister,queue_size=500)
 
 def showUpdatedRegisters(msg):
     rospy.loginfo("Modbus server registers have been updated: %s",str(msg.data))
@@ -40,7 +42,77 @@ def showUpdatedRegisters(msg):
         command.rSPA = 255
         command.rFRA = 150
 
-    pub.publish(command)
+    pub_gripper.publish(command)
+
+def statusInterpreter(status):
+
+    ## GRIPPER STATUS
+    # Initialization status, echo of the rACT bit (activation bit).
+    gACT = status.gACT
+
+    # Operation Mode status, echo of the rMOD bits (grasping mode requested).
+    gMOD = status.gMOD
+
+    # Action status, echo of the rGTO bit (go to bit).
+    gGTO = status.gGTO
+
+    # Gripper status, returns the current status of the Gripper.
+    gIMC = status.gIMC
+
+    # Gripper status, returns the current status & motion of the Gripper fingers.
+    gSTA = status.gSTA
+
+    ## OBJECT STATUS
+    gDTA = status.gDTA
+    gDTB = status.gDTB
+    gDTC = status.gDTC
+    gDTS = status.gDTS
+
+    ## FAULT STATUS
+    gFLT = status.gFLT
+
+    ## POSITION REQUEST ECHO (FINGER A IN INDIVIDUAL MODE)
+    gPRA = status.gPRA
+
+    ## FINGER A POSITION
+    gPOA = status.gPOA
+
+    ## FINGER A CURRENT
+    gCUA = status.gCUA
+
+    ## FINGER B POSITION REQUEST ECHO 
+    gPRB = status.gPRB
+
+    ## FINGER B POSITION
+    gPOB = status.gPOB
+
+    ## FINGER B CURRENT
+    gCUB = status.gCUB
+
+    ## FINGER C POSITION REQUEST ECHO
+    gPRC = status.gPRC
+
+    ## FINGER C POSITION
+    gPOC = status.gPOC
+
+    ## FINGER C CURRENT
+    gCUC = status.gCUC
+
+    ## SCISSOR POSITION REQUEST ECHO
+    gPRS = status.gPRS
+
+    ## SCISSOR POSITION
+    gPOS = status.gPOS
+
+    ## SCISSOR CURRENT
+    gCUS = status.gCUS
+
+    output = HoldingRegister()
+    output.data = 1
+    
+    rospy.loginfo("Sending arrays to the modbus server")
+
+    pub_robot.publish(output)
 
 
 if __name__=="__main__":
@@ -70,7 +142,8 @@ if __name__=="__main__":
     # Create a listener that show us a message if anything on the readable modbus registers change
     rospy.loginfo("All done. Listening to inputs... Terminate by Ctrl+c")
 
-    sub = rospy.Subscriber("modbus_wrapper/input",HoldingRegister,showUpdatedRegisters,queue_size=500)
+    sub_fanuc = rospy.Subscriber("modbus_wrapper/input",HoldingRegister,showUpdatedRegisters,queue_size=500)
+    sub_robotiq = rospy.Subscriber("SModelRobotInput", inputMsg.SModel_robot_input, printStatus, queue_size=500) 
 
     # Spins ROS
     rospy.spin()    
